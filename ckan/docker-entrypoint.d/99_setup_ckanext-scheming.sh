b2877527325b32013e599f60957e3384e393a10d
Admir Trakic <atrakic@users.noreply.github.com>
2025-12-22 02:13:47 +0100

    Update actions

--- ckan/docker-entrypoint.d/99_setup_ckanext-scheming.sh
+++ ckan/docker-entrypoint.d/99_setup_ckanext-scheming.sh
@@ -7,7 +7,7 @@ if [[ $CKAN__PLUGINS == *"scheming_datasets"* ]]; then
    # Include both ckanext-scheming and ckanext-dcat presets
    ckan config-tool $CKAN_INI "scheming.presets = ckanext.scheming:presets.json ckanext.dcat.schemas:presets.yaml"
    # Configure HealthDCAT-AP RDF profile
-   ckan config-tool $CKAN_INI "ckanext.dcat.rdf.profiles = euro_dcat_ap euro_health_dcat_ap" 
+   ckan config-tool $CKAN_INI "ckanext.dcat.rdf.profiles = euro_dcat_ap euro_health_dcat_ap"
  else
    echo "Not configuring scheming_datasets"
 fi
